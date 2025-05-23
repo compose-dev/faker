6ae38b1030dbecc03bf3e1454c1b501bf73f2f1c
Atul Jalan <atul@composehq.com>
2025-05-22 19:40:22 -0500

    update records functionality

--- src/record.ts
+++ src/record.ts
@@ -1,90 +1,122 @@
 import * as primitives from "./primitives";
 
-interface BaseField {
+interface BaseDefinition {
   type: string;
-  key: string;
 }
 
-interface PersonNameField extends BaseField {
-  type: "person_name";
+const TYPES = {
+  personName: "personName",
+  companyName: "companyName",
+  int: "int",
+  float: "float",
+  email: "email",
+  featureFlags: "featureFlags",
+} as const;
+
+type Type = (typeof TYPES)[keyof typeof TYPES];
+
+interface PersonNameDefinition extends BaseDefinition {
+  type: typeof TYPES.personName;
 }
 
-interface CompanyNameField extends BaseField {
-  type: "company_name";
+interface CompanyNameDefinition extends BaseDefinition {
+  type: typeof TYPES.companyName;
 }
 
-interface IntField extends BaseField {
-  type: "int";
+interface IntDefinition extends BaseDefinition {
+  type: typeof TYPES.int;
   min: number;
   max: number;
 }
 
-interface FloatField extends BaseField {
-  type: "float";
+interface FloatDefinition extends BaseDefinition {
+  type: typeof TYPES.float;
   min: number;
   max: number;
   decimals: number;
 }
 
-interface EmailField extends BaseField {
-  type: "email";
+interface EmailDefinition extends BaseDefinition {
+  type: typeof TYPES.email;
 }
 
-interface FeatureFlagsField extends BaseField {
-  type: "feature_flags";
+interface FeatureFlagsDefinition extends BaseDefinition {
+  type: typeof TYPES.featureFlags;
   count: number;
 }
 
-type Field =
-  | PersonNameField
-  | CompanyNameField
-  | IntField
-  | FloatField
-  | EmailField
-  | FeatureFlagsField;
+type FieldDefinition =
+  | PersonNameDefinition
+  | CompanyNameDefinition
+  | IntDefinition
+  | FloatDefinition
+  | EmailDefinition
+  | FeatureFlagsDefinition
+  | Type;
 
-type ReturnType<T extends Field[]> = {
-  [K in T[number]["key"]]: T[number]["type"] extends
-    | "person_name"
-    | "company_name"
-    | "email"
-    ? string
-    : T[number]["type"] extends "int" | "float"
-      ? number
-      : T[number]["type"] extends "feature_flags"
-        ? Record<string, boolean | number | string>
-        : never;
+type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
+
+type ReturnType<T extends Record<string, FieldDefinition | string>> = {
+  [K in keyof T]: T[K] extends FieldDefinition
+    ? T[K] extends { type: typeof TYPES.personName } | typeof TYPES.personName
+      ? string
+      : T[K] extends
+            | { type: typeof TYPES.companyName }
+            | typeof TYPES.companyName
+        ? string
+        : T[K] extends { type: typeof TYPES.int } | typeof TYPES.int
+          ? number
+          : T[K] extends { type: typeof TYPES.float } | typeof TYPES.float
+            ? number
+            : T[K] extends { type: typeof TYPES.email } | typeof TYPES.email
+              ? string
+              : T[K] extends
+                    | { type: typeof TYPES.featureFlags }
+                    | typeof TYPES.featureFlags
+                ? Record<string, string | number | boolean>
+                : never
+    : T[K] extends string
+      ? string
+      : never;
 };
 
-function generateRecord<T extends Field[]>(fields: T): ReturnType<T> {
+function generateRecord<T extends Record<string, FieldDefinition>>(
+  fields: T
+): Prettify<ReturnType<T>> {
   const row: Record<string, any> = {};
 
   const personName = primitives.personName();
 
-  for (const field of fields) {
-    switch (field.type) {
-      case "person_name":
-        row[field.key] = personName.full;
-        break;
-      case "int":
-        row[field.key] = primitives.int(field.min, field.max);
-        break;
-      case "float":
-        row[field.key] = primitives.float(field.min, field.max, field.decimals);
-        break;
-      case "email":
-        row[field.key] = primitives.email(personName);
-        break;
-      case "feature_flags":
-        row[field.key] = primitives.featureFlags(field.count);
-        break;
+  for (const [key, field] of Object.entries(fields)) {
+    const object: Record<string, any> =
+      "type" in (field as any) ? (field as any) : { type: field };
+
+    if (object.type === TYPES.personName) {
+      row[key] = personName.full;
+    } else if (object.type === TYPES.companyName) {
+      row[key] = primitives.companyName();
+    } else if (object.type === TYPES.int) {
+      row[key] = primitives.int(object.min, object.max);
+    } else if (object.type === TYPES.float) {
+      row[key] = primitives.float(object.min, object.max, object.decimals);
+    } else if (object.type === TYPES.email) {
+      row[key] = primitives.email(personName);
+    } else if (object.type === TYPES.featureFlags) {
+      row[key] = primitives.featureFlags(object.count);
+    } else {
+      throw new Error(`Unknown field type: ${object.type}`);
     }
   }
 
   return row as ReturnType<T>;
 }
 
-function generateRecords<T extends Field[]>(fields: T, count: number) {
+function generateRecords<T extends Record<string, FieldDefinition>>(
+  fields: T,
+  count: number
+) {
   const rows: ReturnType<T>[] = [];
 
   for (let i = 0; i < count; i++) {
